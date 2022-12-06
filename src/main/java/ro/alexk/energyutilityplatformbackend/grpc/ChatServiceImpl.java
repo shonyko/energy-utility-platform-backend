17cdbc912a8709f5ddb081d7c19fe0919e8951ed
Alex Kovacs <alexkovacs@rocketmail.com>
2022-12-06 02:52:11 +0200

    Implement grpc service methods

--- src/main/java/ro/alexk/energyutilityplatformbackend/grpc/ChatServiceImpl.java
+++ src/main/java/ro/alexk/energyutilityplatformbackend/grpc/ChatServiceImpl.java
@@ -1,25 +1,62 @@
 package ro.alexk.energyutilityplatformbackend.grpc;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.lognet.springboot.grpc.GRpcService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @GRpcService
 public class ChatServiceImpl extends ChatServiceGrpc.ChatServiceImplBase {
 
+    Map<String, StreamObserver<Message>> subscriptions = new HashMap<>();
+
     @Override
-    public void sendMessage(Message request, StreamObserver<Message> responseObserver) {
-        System.out.println("Got: " + request.getMessage());
-        responseObserver.onNext(request);
-        responseObserver.onCompleted();
+    public void subscribeToChat(Subscription request, StreamObserver<Message> responseObserver) {
+        log.info(request.getId() + " has subscribed to the chat.");
+        subscriptions.put(request.getId(), responseObserver);
+    }
+
+    @Override
+    public void unsubscribeFromChat(Subscription request, StreamObserver<nothing> responseObserver) {
+        log.info(request.getId() + " has unsubscribed to the chat.");
+        var stream = subscriptions.remove(request.getId());
+        if (stream == null) return;
+        stream.onCompleted();
+    }
+
+    @Override
+    public void sendMessage(ChatMessage request, StreamObserver<nothing> responseObserver) {
+        log.info(request.getFrom() + " has messaged " + request.getTo());
+        subscriptions.get(request.getTo()).onNext(
+                Message.newBuilder()
+                        .setMessageType(MessageType.CHAT_MESSAGE)
+                        .setChatMessage(request)
+                        .build()
+        );
     }
 
     @Override
     public void sendTypingNotification(TypingNotification request, StreamObserver<nothing> responseObserver) {
-        super.sendTypingNotification(request, responseObserver);
+        log.info(request.getFrom() + " is typing to " + request.getTo());
+        subscriptions.get(request.getTo()).onNext(
+                Message.newBuilder()
+                        .setMessageType(MessageType.TYPING_NOTIFICATION)
+                        .setTypingNotification(request)
+                        .build()
+        );
     }
 
     @Override
     public void sendMessageReadNotification(MessageReadNotification request, StreamObserver<nothing> responseObserver) {
-        super.sendMessageReadNotification(request, responseObserver);
+        log.info(request.getFrom() + " read messages from " + request.getTo());
+        subscriptions.get(request.getTo()).onNext(
+                Message.newBuilder()
+                        .setMessageType(MessageType.MESSAGE_READ_NOTIFICATION)
+                        .setMessageReadNotification(request)
+                        .build()
+        );
     }
 }
